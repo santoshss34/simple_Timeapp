8c34b0aa111dee6505ee4d84d910686782eea281
santoshss34 <120577953+santoshss34@users.noreply.github.com>
2025-05-15 15:32:45 +0530

    terraform variables file from that we write main file

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "docker_image" {
+  description = "santosh2231/simpletimeservice"
+}
